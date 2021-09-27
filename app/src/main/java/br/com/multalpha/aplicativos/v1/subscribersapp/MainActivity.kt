c1ba40c36ff82400f9c4688b38ac11af9faa4235
John Lima <ti.junior@gmail.com>
2021-09-27 16:14:28 -0300

    commit start

---
+++ app/src/main/java/br/com/multalpha/aplicativos/v1/subscribersapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.multalpha.aplicativos.v1.subscribersapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
