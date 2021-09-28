653a916aa4bf7b1eb19e875db1ef073165ae4c25
John Lima <ti.junior@gmail.com>
2021-09-28 15:31:38 -0300

    Criação e Configuração da Toolbar

--- app/src/main/java/br/com/multalpha/aplicativos/v1/subscribersapp/MainActivity.kt
+++ app/src/main/java/br/com/multalpha/aplicativos/v1/subscribersapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package br.com.multalpha.aplicativos.v1.subscribersapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Configuração do NavFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar_main.setupWithNavController(navController, appBarConfiguration)
     }
 }
