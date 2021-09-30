9f7a74e06d37f352be306e44926520684d14105c
John Lima <ti.junior@gmail.com>
2021-09-30 10:33:54 -0300

    Configuração da Tela de Listagem Subscribers

---
+++ app/src/main/java/br/com/multalpha/aplicativos/v1/subscribersapp/ui/subscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,10 @@
+package br.com.multalpha.aplicativos.v1.subscribersapp.ui.subscriberlist
+
+import androidx.fragment.app.Fragment
+import br.com.multalpha.aplicativos.v1.subscribersapp.R
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+    private lateinit var viewModel: SubscriberListViewModel
+
+}
